cefa40648e6f9694b47ec09ef9295218929b5a6b
Sven Herzky <sven@e7o.de>
2020-03-18 10:32:16 +0100

    Allowing SQLite connections

--- src/e7o/Moments/Database/Connection.php
+++ src/e7o/Moments/Database/Connection.php
@@ -2,14 +2,37 @@
 
 namespace e7o\Moments\Database;
 
+/**
+* Opens a database connection, where $config contains an array containing some
+* of the following options as usual:
+* 
+* - dsn -- just the plain DSN string
+* - type -- like "sqlite" or "mysql"
+* - host, dbname, charset # for e.g. mysql
+* - file # for sqlite; should be something like "${root}/data/storage.sqlite"
+*   (absolute path required by sqlite connector)
+*/
 class Connection extends \PDO
 {
 	protected $config;
 	
 	public function __construct($config)
 	{
-		$dsn = 'mysql:dbname=' . $config['database'] . ';host=' . $config['host'] . ';charset=' . $config['charset'];
-		parent::__construct($dsn, $config['user'], $config['password']);
+		switch ($config['type']) {
+			case 'sqlite':
+				$dsn = 'sqlite:' . $config['file'];
+				$usr = null;
+				$pw = null;
+				break;
+			default:
+				$dsn = ($config['type'] ?? 'mysql') . ':dbname=' . $config['database']
+					. ';host=' . $config['host'] . ';charset=' . $config['charset']
+				;
+				$usr = $config['user'];
+				$pw = $config['password'];
+		}
+		
+		parent::__construct($dsn, $usr, $pw);
 	}
 }
 
